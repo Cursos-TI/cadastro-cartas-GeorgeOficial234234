2fcc6f29b1155bd7839a73052c80df216c7ca7d5
GeorgeOficial234234 <georgeantonio420@gmail.com>
2026-01-03 21:24:42 +0000

    Desafio: Nível Novato

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,11 +5,47 @@
 // Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
 
 int main() {
-  // Área para definição das variáveis para armazenar as propriedades das cidades
+  // NÍVEL NOVATO
+  
+// VARIÁVEIS
+  char state;
+  char cardNumber[2];
+  char cityName[10];
+  int population;
+  float areaKm;
+  float pIB;
+  int turisticPoints;
 
-  // Área para entrada de dados
+// CHAMADAS
+  printf("Insira uma LETRA (de 'A' a 'H') para estado:\n");
+  scanf("%c", &state);
 
-  // Área para exibição dos dados da cidade
+  printf("\nInsira o CÓDIGO (de '1' a '4'):\n");
+  scanf("%1s", cardNumber); // %1s limita à apenas um dígito exibido no terminal
+
+  printf("\nInsira o NOME da cidade:\n");
+  scanf("%s", cityName); // Em arrays já não é necessário o uso de '&'
+
+  printf("\nInsira um valor de POPULAÇÃO:\n");
+  scanf("%d", &population);
+
+  printf("\nInsira um valor para ÁREA:\n");
+  scanf("%f", &areaKm);
+
+  printf("\nInsira um valor para PIB:\n");
+  scanf("%f", &pIB);
+
+  printf("\nInsira uma quantidade de PONTOS TURÍSTICOS:\n");
+  scanf("%d", &turisticPoints);
+
+// TERMINAL 
+  printf("\nEstado: %c \n", state);
+  printf("Código da Carta: %c0%s \n", state, cardNumber);
+  printf("Nome da Cidade: %s \n", cityName);
+  printf("População: %d \n", population);
+  printf("Área (em km²): %f \n", areaKm);
+  printf("PIB: %f \n", pIB);
+  printf("Número de Pontos Turísticos: %d \n", turisticPoints);
 
 return 0;
 } 
